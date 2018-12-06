ab04ee31f4b1d0cfa7a87bd1ab2a04577439b3f4
manikoth <colin@manikoth.com>
2018-12-06 01:04:10 -0800

    III. Service Worker

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
-var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+// Detect Service Worker
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then(function(cache) {
+      cache.addAll(['/', '/index.html', '/js/scripts.js', '/manifest.json']);
+    })
+  );
+});
 
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(res) {
+          return caches.open('dynamic').then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    })
+  );
+});
+
+
+
+// Carousel
+var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
 
 // Primary
 $.simpleWeather({
